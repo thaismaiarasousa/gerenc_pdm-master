f75d8af8ab2bd21d43f2c6623f8e1108389962c4
Tadeu Leite <42976398+tadeuleite@users.noreply.github.com>
2024-09-08 19:27:50 +0000

    refazendo master

--- ciapedemanga/src/environments/environment.production.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    tokenGraph: 'IGQWROaEtSQXE4ZAW56cXFkaDZA4RXl3d1BJV1dmSnFMLWJoVUNycnR2T2V3aklxNFBpcDIzREdCWEZASRVdPamRNSUtyQUpkUkpRNFVORFlQNW5wdnhQenFEazRsZA2hucFdGM3czWjVqVzFRbGZAjdVRQWXZAtOHgyYlUZD'
-  };
-  
